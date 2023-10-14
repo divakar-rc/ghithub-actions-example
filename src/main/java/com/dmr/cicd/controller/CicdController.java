261fa7396adbbaa063dd749d7132e213009027db
divakar-git <divakarrc1991@gmail.com>
2023-10-13 23:54:26 -0400

    first commit

---
+++ src/main/java/com/dmr/cicd/controller/CicdController.java
@@ -0,0 +1,13 @@
+package com.dmr.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CicdController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Intellij";
+    }
+}
